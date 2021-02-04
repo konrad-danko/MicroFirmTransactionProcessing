114445353149f86f069bc985409f60b8db298b56
Konrad Dańko <konrad.danko@gmail.com>
2021-02-04 23:13:19 +0100

    Commit of DisplayParams class creation

---
+++ src/main/java/pl/coderslab/MicroFirm/controller/DisplayParams.java
@@ -0,0 +1,38 @@
+package pl.coderslab.MicroFirm.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+
+@Component
+public class DisplayParams {
+
+    private final static String DISABLED_PARAM = "disabledParam";
+    private final static String SUBMIT_BTN_VISIBLE_PARAM = "submitBtnVisibleParam";
+    private final static String EDIT_BTN_VISIBLE_PARAM = "editBtnVisibleParam";
+    private final static String DEL_BTN_VISIBLE_PARAM = "delBtnVisibleParam";
+    private final static String TRUE = "true";
+    private final static String FALSE = "false";
+    private final static String INVISIBLE = "invisible";
+    private final static String VISIBLE = "visible";
+
+    public void setShowParams(Model model){
+        model.addAttribute(DISABLED_PARAM, TRUE);
+        model.addAttribute(SUBMIT_BTN_VISIBLE_PARAM, INVISIBLE);
+        model.addAttribute(EDIT_BTN_VISIBLE_PARAM, VISIBLE);
+        model.addAttribute(DEL_BTN_VISIBLE_PARAM, VISIBLE);
+    }
+
+    public void setAddEditParams(Model model){
+        model.addAttribute(DISABLED_PARAM, FALSE);
+        model.addAttribute(SUBMIT_BTN_VISIBLE_PARAM, VISIBLE);
+        model.addAttribute(EDIT_BTN_VISIBLE_PARAM, INVISIBLE);
+        model.addAttribute(DEL_BTN_VISIBLE_PARAM, INVISIBLE);
+    }
+
+    public void setDelParams(Model model){
+        model.addAttribute(DISABLED_PARAM, TRUE);
+        model.addAttribute(SUBMIT_BTN_VISIBLE_PARAM, VISIBLE);
+        model.addAttribute(EDIT_BTN_VISIBLE_PARAM, INVISIBLE);
+        model.addAttribute(DEL_BTN_VISIBLE_PARAM, INVISIBLE);
+    }
+}
